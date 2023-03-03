ba5b8ab791c2458a4f54a7dfd05ded793c4fa0ee
Aaditya Sudhir Joshi <64467870+Aaditya50@users.noreply.github.com>
2023-03-03 22:55:45 +0530

    Create swap-without-using-third-variable.java

---
+++ Java-basic-code-practice/swap-without-using-third-variable.java
@@ -0,0 +1,12 @@
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+       int a=10;
+       int b=15;
+        b=a+b;
+        a=b-a;
+        b=b-a;
+       System.out.println(a+" "+b);
+    }
+}
